585178a048e2320af45f12fb9c6f5bb7b8b8c281
Paulina Rzemieniarz <p.rzemieniarz@gmail.com>
2025-07-02 19:49:55 +0200

    initial backend setup

---
+++ Backend/Program.cs
@@ -0,0 +1,29 @@
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference(options => options
+    .WithTitle("Job Apply Helper")
+    .WithTheme(ScalarTheme.Saturn)
+    .WithDarkMode());
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
